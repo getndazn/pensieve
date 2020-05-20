ab538e2958c4d339ec8fa76588a702de3c5ad1c1
Sara Gerion <sara.gerion@dazn.com>
2020-05-20 10:38:39 +0200

    build: added github workflow

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,7 +3,6 @@ const tasks = t => t.join(' && ')
 module.exports = {
     hooks: {
         'pre-commit': tasks([
-          './drone-jsonnet/verify.sh',
           'npm run lint-check',
           'npm run build',
           'npm run test',
